a6bf26805e457df8d0c8275fa69f3c0c82ba2361
Gileno Filho <contato@gilenofilho.com.br>
2017-03-13 16:30:29 -0300

    aula 09

--- modulo-02/olx/olx/spiders/cars.py
+++ modulo-02/olx/olx/spiders/cars.py
@@ -5,10 +5,19 @@ import scrapy
 class CarsSpider(scrapy.Spider):
 
     name = "cars"
-    allowed_domains = ["pe.olx.com.br/veiculos/carros"]
+    allowed_domains = ["pe.olx.com.br"]
     start_urls = ['http://pe.olx.com.br/veiculos/carros/']
 
     def parse(self, response):
-        items = response.xpath('//ul[@id="main-ad-list"]/li')
+        # items = response.xpath(
+        #     '//ul[@id="main-ad-list"]/li[contains(@class, "item") and not(contains(@class, "list_native"))]'
+        # )
+        items = response.xpath(
+            '//ul[@id="main-ad-list"]/li[not(contains(@class, "list_native"))]'
+        )
         for item in items:
-            self.log(item.xpath('./a/@href').extract_first())
+            url = item.xpath('./a/@href').extract_first()
+            yield scrapy.Request(url=url, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        self.log(response.url)
