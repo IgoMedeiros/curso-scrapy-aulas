0df39d01674ede51bfc8b06b5365fe5946829a3e
Gileno Filho <contato@gilenofilho.com.br>
2017-03-27 16:28:58 -0300

    adicionando item pipeline

--- modulo-02/olx/olx/settings.py
+++ modulo-02/olx/olx/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'olx.pipelines.OlxPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'olx.pipelines.OlxPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
