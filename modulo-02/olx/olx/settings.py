2ccfa23b277e56c954d2d139dc0885ea635c7d5f
Gileno Filho <contato@gilenofilho.com.br>
2017-05-01 16:04:39 -0300

    aula 16

--- modulo-02/olx/olx/settings.py
+++ modulo-02/olx/olx/settings.py
@@ -65,7 +65,8 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'olx.pipelines.OlxSqlitePipeline': 300,
+   #'olx.pipelines.OlxSqlitePipeline': 300,
+   'olx.pipelines.MongoPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -88,3 +89,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI = 'localhost'
+MONGO_DATABASE = "olx"
