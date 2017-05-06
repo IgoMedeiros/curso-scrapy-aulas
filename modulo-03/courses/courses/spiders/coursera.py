0ac69b255323e27995e8e972caddcd771332514b
Gileno Filho <contato@gilenofilho.com.br>
2017-05-06 14:17:05 -0300

    adicionando projeto courses no modulo 03

---
+++ modulo-03/courses/courses/spiders/coursera.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class CourseraSpider(scrapy.Spider):
+
+    name = "coursera"
+    start_urls = ['https://www.coursera.org/browse?languages=pt']
+
+    def parse(self, response):
+        self.log('Hello World! Scrapy Project')
