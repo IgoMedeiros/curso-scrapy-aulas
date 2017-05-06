6a9862bb08e38b3e6fb802221736f7105988d328
Gileno Filho <contato@gilenofilho.com.br>
2017-05-06 14:38:58 -0300

    aula 18

--- modulo-03/courses/courses/spiders/coursera.py
+++ modulo-03/courses/courses/spiders/coursera.py
@@ -7,4 +7,14 @@ class CourseraSpider(scrapy.Spider):
     start_urls = ['https://www.coursera.org/browse?languages=pt']
 
     def parse(self, response):
-        self.log('Hello World! Scrapy Project')
+        categories = response.xpath("//div[contains(@class, 'rc-DomainNav')]/a")
+        for cat in categories:
+            cat_url = cat.xpath('./@href').extract_first()
+            self.log('Category: %s' % cat_url)
+            yield scrapy.Request(
+                url='https://www.coursera.org%s' % cat_url,
+                callback=self.parse_category
+            )
+    
+    def parse_category(self, response):
+        self.log(response.xpath("//title/text()").extract_first())
