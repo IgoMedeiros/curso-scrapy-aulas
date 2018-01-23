d91c903f88d33d527738e4b78ebfe020909af649
Gileno Filho <contato@gilenofilho.com.br>
2018-01-23 00:20:27 -0300

    aula 21

--- modulo-03/courses/courses/items.py
+++ modulo-03/courses/courses/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class CoursesItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class CourseItem(scrapy.Item):
+    
+    title = scrapy.Field()
+    headline = scrapy.Field()
+    url = scrapy.Field()
+    instructors = scrapy.Field()
+    lectures = scrapy.Field()
+    image = scrapy.Field()
